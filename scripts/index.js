9d2112ffa4d02608633581f9360ec4b511e224b7
AlekseyPchelintsev <alexey.pchelincev@yandex.ru>
2024-07-05 17:29:26 +0300

    save

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
-
+const content = document.querySelector(".content");
+const places = document.querySelector(".places__list");
 // @todo: Функция создания карточки
+function createCard() {
+  initialCards.forEach((element) => {
+    const cardElement = cardTemplate
+      .querySelector(".places__item")
+      .cloneNode(true);
+    cardElement.querySelector("img").src = element.link;
+    cardElement.querySelector("img").alt = element.name;
+    cardElement.querySelector(".card__title").textContent = element.name;
+    places.append(cardElement);
+  });
+  const deleteButtonCard = content.querySelector(".card__delete-button");
 
+  deleteButtonCard.addEventListener("click", deleteCard);
+}
 // @todo: Функция удаления карточки
 
+function deleteCard() {
+  const removeCard = buttonDeleteCard.closest(".places__item");
+  removeCard.remove();
+}
 // @todo: Вывести карточки на страницу
+
+createCard();
